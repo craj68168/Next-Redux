ed77d7525f3857bc298cc9719229ea49857589c7
craj68168 <craj68168@gmail.com>
2022-09-16 14:31:27 +0545

    persis reducer store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { wrapper } from "../store";
+import { wrapper, persistor } from "../store";
 import { useEffect, useState } from "react";
-
+import { PersistGate } from "redux-persist/integration/react";
 function MyApp({ Component, pageProps }: AppProps) {
   const [showChild, setShowChild] = useState(false);
 
@@ -13,7 +13,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   if (!showChild) {
     return null;
   }
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+      </PersistGate>
+    </>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
--- store/index.tsx
+++ store/index.tsx
@@ -2,11 +2,15 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import counter from "./slices/counterSlice";
 import user from "../store/slices/userSlice";
 import { createWrapper, HYDRATE } from "next-redux-wrapper";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import thunk from 'redux-thunk';
 
 const combineReducer = combineReducers({
   counter,
   user,
 });
+
 
 const masterReducer = (state: any, action: any) => {
   if (action.type === HYDRATE) {
@@ -25,11 +29,18 @@ const masterReducer = (state: any, action: any) => {
     return combineReducer(state, action);
   }
 };
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, masterReducer);
 
 const makeStore: any = () =>
   configureStore({
-    reducer: masterReducer,
+    reducer: persistedReducer,
+    middleware: [thunk]
   });
+ export const persistor = persistStore(makeStore())
 
 export const wrapper = createWrapper(makeStore, { debug: true });
 export type RootState = ReturnType<typeof makeStore.getState>;
