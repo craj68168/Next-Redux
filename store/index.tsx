7534603e42aebcfd7797f16bbbaf611be4772aa2
craj68168 <craj68168@gmail.com>
2022-09-20 11:52:40 +0545

    redux logger done

--- store/index.tsx
+++ store/index.tsx
@@ -1,4 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware } from 'redux';
+import logger from 'redux-logger'
 import user from "../store/slices/userSlice";
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
@@ -12,7 +14,7 @@ const persistedUser = persistReducer(persistConfig, user);
 export const store =  configureStore({
   reducer: persistedUser,
   devTools: process.env.NODE_ENV !== 'production',
-  middleware: [thunk]
+  middleware: [thunk,logger]
 });
 export const persistor = persistStore(store)
 
